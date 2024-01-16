8969a229cc5db55f828c57f39bd385a440ab06f4
Akshat <akshath.centaurus@gmail.com>
2024-01-16 20:03:08 +0530

    Added Chat Functionality to All the chats

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -6,6 +6,8 @@ import { MessageCircleIcon } from "lucide-react";
 import { Session } from "next-auth";
 import { createRef, useEffect } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import LoadingSpinner from "./LoadingSpinner";
+import UserAvataar from "./UserAvataar";
 
 function ChatMessages({
     chatId,
@@ -34,7 +36,7 @@ function ChatMessages({
   <div className="p-5">
      
       {!loading && messages?.length ===0 && (
-        <div className="flex flex-col justify-center  text-center items-center p-20 rounded-xl space-y-2 bg-blue-600 text-black ">
+        <div className="flex flex-col justify-center  text-center items-center p-20 rounded-xl space-y-2 bg-blue-500 text-black ">
           <MessageCircleIcon className="h-10 w-10"/>
           <h2> 
             <span className="font-bold">Invite a friend </span>&{" "}
@@ -46,6 +48,40 @@ function ChatMessages({
           <p className="font-bold">Edoc will auto-detect and tranlate it all for you</p>
           </div>
       )}
+
+      { messages?.map((message) => {
+        const isSender = message.user.id === session?.user.id;
+
+        return (
+          <div key={message.id} className="flex my-2 items-end">
+            <div 
+              className={`flex flex-col relative space-y-2 p-4 w-fit line-clamp-1 mx-2 rounded-lg ${
+                isSender ? "ml-auto bg-blue-500 text-white rounded-br-none" : "bg-gray-100 dark:text-gray-100 dark:bg-slate-700 rounded-bl-none"}`}
+            
+              >
+                <p className={`text-xs italic font-extralight line-clamp-1 ${
+                  isSender ? "text-right" : "text-left"
+                }`}
+                >
+                  {message.user.name.split(" ")[0]}
+                </p>
+                <div className="flex space-x-2">
+                  <p>{message.translated?.[language]|| message.input}</p>
+                  {!message.translated && <LoadingSpinner />}
+                </div>
+                </div>
+
+                <UserAvataar
+                  name={message.user.name}
+                  image={message.user.image}
+                  className={`${!isSender && "-order-1"}`}
+                  />
+                  </div>
+        );
+      })}
+
+
+
     </div>
   );
   
