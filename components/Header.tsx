3682c42fb10d708acb344327014065dcfec7e64f
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-01-20 22:48:25 +0530

    frontend changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,11 +12,11 @@ import LanguageSelect from "./LanguageSelect";
 async function Header() {
   const session = await getServerSession(authOptions);
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
-        <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl">
+    <header className="sticky top-0 z-10 bg-white dark:bg-gray-900">
+        <nav className="flex flex-col items-center sm:flex-row sm:items-center bg-white mt-8 mr-6 ml-6 mb-6 dark:bg-gray-900 max-w-7xl">
             <Logo/>
 
-            <div className="flex-1 flex items-center justify-end space-x-4">  
+            <div className="flex-1 flex items-center justify-end ">  
             <LanguageSelect />
 
               {session ? (
