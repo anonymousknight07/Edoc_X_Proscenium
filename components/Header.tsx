29fc14a63bdeb0be66cf2cdf7cc495886c76488e
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-01-21 02:24:36 +0530

    minor changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import DarkModeToggle from "./DarkModeToggle";
 import Logo from "./Logo";
 import UserButton from "./UserButton";
 import { authOptions } from "@/auth";
-import {  MessageSquareIcon } from "lucide-react";
+import { MessageSquareIcon } from "lucide-react";
 import Link from "next/link";
 import CreateChatButton from "./CreateChatButton";
 import UpgradeBanner from "./UpgradeBanner";
@@ -13,30 +13,30 @@ async function Header() {
   const session = await getServerSession(authOptions);
   return (
     <header className="sticky top-0 z-10 bg-white dark:bg-gray-900">
-        <nav className="flex flex-col items-center sm:flex-row sm:items-center bg-white mt-8 mr-6 ml-6 mb-6 dark:bg-gray-900 max-w-7xl">
-            <Logo/>
+      <nav className="flex flex-col items-center sm:flex-row sm:items-center bg-white mt-8 mr-6 ml-6 mb-6 dark:bg-gray-900 max-w-7xl">
+        <Logo />
 
-            <div className="flex-1 flex items-center justify-end gap-3">  
-            <LanguageSelect />
+        <div className="flex-1 flex items-center justify-end gap-3">
+          <LanguageSelect />
 
-              {session ? (
-                <>
-                     <Link 
-                     href={'/chat'} prefetch={false}> 
-                     <MessageSquareIcon className="text-black dark:text-white"/>
-                     </Link>
-                     <CreateChatButton/>
-                </>
-              ) : (
-                <Link href="/pricing">Subscription Plans</Link>
-              )}
-              <DarkModeToggle />
-              <UserButton session={session}/>
+          {session ? (
+            <>
+              <Link
+                href={'/chat'} prefetch={false}>
+                <MessageSquareIcon className="text-black dark:text-white" />
+              </Link>
+              <CreateChatButton />
+            </>
+          ) : (
+            <Link href="/pricing">Subscription Plans</Link>
+          )}
+          <DarkModeToggle />
+          <UserButton session={session} />
 
-            </div>
-        </nav>
+        </div>
+      </nav>
 
-        <UpgradeBanner />
+      <UpgradeBanner />
     </header>
   );
 }
